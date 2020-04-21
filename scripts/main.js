df43183ebd642f92460baac5425196241876bf58
Noel-Garces08 <noel.garces08@gmail.com>
2020-04-21 12:12:11 -0600

    Version Two of lily Photography

---
+++ scripts/main.js
@@ -0,0 +1,33 @@
+const navSlide = () => {
+	const burger = document.querySelector('.burger');
+	const nav = document.querySelector('.nav-links');
+	const navLinks = nav.querySelectorAll('li a');
+
+	burger.addEventListener('click', () => {
+		// Toggle Nav
+		nav.classList.toggle('nav-active');
+
+		// Burger Animation
+		burger.classList.toggle('toggle');
+
+		// Prevent scrolling on open menu
+		if (!nav.classList.contains('nav-active')) {
+			document.querySelector('body').style.overflowY = '';
+		} else {
+			document.querySelector('body').style.overflowY = 'hidden';
+		}
+
+		//Animate Links
+		navLinks.forEach((link, index) => {
+			if (link.style.animation) {
+				link.style.animation = '';
+			} else {
+				link.style.animation = `navLinkFadeIn 0.5s ease forwards ${
+					index / 7
+				}s`;
+			}
+		});
+	});
+};
+
+navSlide();
